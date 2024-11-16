0524f5f064eeba1b27698a27656024b46a26f9b6
Meda Varun Kumar <codedotgo@gmail.com>
2024-11-16 13:28:53 +0530

    feat: list of objects added

--- Proj01_1-BootJpaCRUDOperationsSAVE_ALL/src/main/java/com/cdc/runners/CrudRepoTestRunner.java
+++ Proj01_1-BootJpaCRUDOperationsSAVE_ALL/src/main/java/com/cdc/runners/CrudRepoTestRunner.java
@@ -1,5 +1,7 @@
 package com.cdc.runners;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -16,15 +18,30 @@ public class CrudRepoTestRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		try {
-			Doctor doctor = new Doctor();
-			doctor.setDocName("Kumar");
-			doctor.setDocIncome(9900.00);
-			doctor.setDocSpecialization("Entemology");
-			String resultMsg = doctorService.registerDoctor(doctor);
-			System.out.println(resultMsg);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		/*		try {
+					Doctor doctor = new Doctor();
+					doctor.setDocName("Kumar");
+					doctor.setDocIncome(9900.00);
+					doctor.setDocSpecialization("Entemology");
+					String resultMsg = doctorService.registerDoctor(doctor);
+					System.out.println(resultMsg);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}*/
+
+		Doctor doc1 = new Doctor();
+		doc1.setDocName("Meda");
+		doc1.setDocIncome(100000.00);
+		doc1.setDocSpecialization("Hemotology");
+		Doctor doc2 = new Doctor();
+		doc2.setDocIncome(20000.00);
+		doc2.setDocName("Rajesh");
+		doc2.setDocSpecialization("Neurology");
+		Doctor doc3 = new Doctor();
+		doc3.setDocIncome(30000.00);
+		doc3.setDocName("antony");
+		doc3.setDocSpecialization("RMP");
+		String msg = doctorService.registerGroupOfDoctors(List.of(doc1, doc2, doc3));
+		System.out.println(msg);
 	}
 }
